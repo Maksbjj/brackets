458df4f1f534b6c69d896fc634c7e8544dd41537
Maxim <maxim3093@gmail.com>
2019-10-12 13:31:42 +0200

    feat: fix solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    let openBrackets = bracketsConfig.map(bracket => bracket[0]);
+    let closeBrackets = bracketsConfig.map(bracket => bracket[1]);
+
+
+    for (let i = 0; i < str.length; i++) {
+        let element = str.charAt(i)
+        if (openBrackets.includes(element)) {
+            stack.push(element)
+        } else if (closeBrackets.includes(element)) {
+            let index = closeBrackets.indexOf(element);
+
+            if (stack[stack.length - 1] === openBrackets[index]) {
+                stack.pop()
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
